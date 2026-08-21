65841089469eb0f73a432447c1e56e2a1494a114
rupeshh7 <rupeshchavan48391@gmail.com>
2026-08-21 13:37:48 +0000

    Move Django secret key to environment variable

--- notesproject/settings.py
+++ notesproject/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/6.1/ref/settings/
 """
 
 from pathlib import Path
-
+import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-^@-3wz(revo!0k^%h-j#j2tkmez1=8xm+7oz74!c1^w3pb#m2&'
+SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY") #'django-insecure-^@-3wz(revo!0k^%h-j#j2tkmez1=8xm+7oz74!c1^w3pb#m2&'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
